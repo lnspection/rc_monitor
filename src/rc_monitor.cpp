30d1269e19234766bff2f09dce39279b592e6d49
Giuseppe Loianno <loiannog@gmail.com>
2016-11-28 17:22:03 -0500

    adding service calls instead of another manager

--- src/rc_monitor.cpp
+++ src/rc_monitor.cpp
@@ -3,6 +3,7 @@
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Empty.h>
 #include <std_msgs/Bool.h>
+#include <mav_manager/Vec4.h>
 #include <memory>
 
 using namespace std;
@@ -14,6 +15,8 @@ float mult_vel = 0.01;
 bool state_hover = true;
 ros::Publisher vel_pub;
 ros::Publisher hov_pub;
+ros::ServiceClient client_vel;
+ros::ServiceClient client_hov;
 
 void rc_callback(const mavros_msgs::RCIn::ConstPtr &RC)
 {
@@ -27,6 +30,7 @@ void rc_callback(const mavros_msgs::RCIn::ConstPtr &RC)
     vel_z = (RC->channels[2] - 1520.0) / 400 * mult_vel;//need to be scaled
   else if(RC->channels[2] <= 1480.0)
     vel_z = (RC->channels[2] - 1480.0) / 400 * mult_vel;//need to be scaled
+
   
   //read channel for the roll to give velocity on y
   if(RC->channels[0] > 1480 && RC->channels[0] < 1520)
@@ -37,7 +41,7 @@ void rc_callback(const mavros_msgs::RCIn::ConstPtr &RC)
     vel_y = (RC->channels[0] - 1480.0) / 400 * mult_vel;//need to be scaled
     
   //call the velocity tracker
-  geometry_msgs::Twist msg_vel;
+  /*geometry_msgs::Twist msg_vel;
   msg_vel.linear.x = 0.0f;
   msg_vel.linear.y = vel_y;
   msg_vel.linear.z = vel_z;
@@ -56,7 +60,51 @@ void rc_callback(const mavros_msgs::RCIn::ConstPtr &RC)
       vel_pub.publish(msg_vel);
       hov_pub.publish(empyt_msg);
     state_hover = true;
-  }    
+  }*/
+  
+  mav_manager::Vec4 srv_vel;
+    srv_vel.request.goal[0]  = 0.0f;
+  srv_vel.request.goal[1]  = vel_y;
+  srv_vel.request.goal[2]  = vel_z;
+  srv_vel.request.goal[3]  = 0.0f;
+  
+    mav_manager::Vec4 srv_hov;
+    srv_hov.request.goal[0]  = 0.0f;
+  srv_hov.request.goal[1]  = 0.0f;
+  srv_hov.request.goal[2]  = 0.0f;
+  srv_hov.request.goal[3]  = 0.0f;
+  
+  if(RC->channels[4] <= 1500){
+
+
+  if (client_vel.call(srv_vel))
+  {
+    ROS_INFO("Sum: %ld", (bool)srv_vel.response.success);
+    state_hover = false;
+  }
+  else
+    ROS_ERROR("Failed to call service velocity");
+  }
+  else if(!state_hover){
+    
+    srv_vel.request.goal[0]  = 0.0f;
+    srv_vel.request.goal[1]  = 0.0f;
+    srv_vel.request.goal[2]  = 0.0f;
+    srv_vel.request.goal[3]  = 0.0f;
+    
+  if (client_vel.call(srv_vel))
+    ROS_INFO("Sum: %ld", (long int)srv_vel.response.success);
+  else
+    ROS_ERROR("Failed to call service velocity before hover");
+   
+  if (client_hov.call(srv_hov))
+    ROS_INFO("Sum: %ld", (bool)srv_hov.response.success);
+  else
+    ROS_ERROR("Failed to call service hover");
+    state_hover = true;
+  }
+  
+  
 }
 
 
@@ -70,10 +118,11 @@ int main(int argc, char **argv)
       //read the external velocity option
       n.param("mult_vel", mult_vel, 0.5f);
       //send the commands
-      vel_pub = n.advertise<geometry_msgs::Twist>("setDesVelInWorldFrame", 10);
-      hov_pub = n.advertise<std_msgs::Empty>("hover", 10);
+      //vel_pub = n.advertise<geometry_msgs::Twist>("setDesVelInWorldFrame", 10);
+      //hov_pub = n.advertise<std_msgs::Empty>("hover", 10);
 
-    
+    client_vel = n.serviceClient<mav_manager::Vec4>("setDesVelInWorldFrame");
+    client_hov = n.serviceClient<mav_manager::Vec4>("hover");
     ros::Subscriber rc_sub_ = n.subscribe("rc/in", 10, &rc_callback,
                                ros::TransportHints().tcpNoDelay());
     
